70ec33440363faa019a352c834f14ed705936e26
muxlisa0206 <sayfullayevamuxlisa27@gmail.com>
2025-08-07 15:05:28 +0500

    adding

--- js/blog.js
+++ js/blog.js
@@ -1,11 +1,12 @@
-const MenuToggle = document.getElementById("toggle")
+const menuToggle = document.getElementById("toggle")
 const close = document.getElementById("close")
 const menus = document.getElementById("menus")
 
 
-MenuToggle.addEventListener("click" , () => {
+menuToggle.addEventListener("click" , () => {
     menus.classList.add("menus_hide")
 })
+
 close.addEventListener("click" , () => {
     menus.classList.remove("menus_hide")
 })
